2b2edf23d2f308b7338b0f4bcf47d0cd1d992962
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2024-08-08 15:37:36 -0400

    year selection box complete

---
+++ src/component/yearList.jsx
@@ -0,0 +1,57 @@
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+} from "@headlessui/react";
+import { useState } from "react";
+import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+
+const years = Array.from({ length: 2024 - 1993 + 1 }, (_, index) => ({
+  year: 1993 + index,
+}));
+
+function YearList() {
+  const [selectedYear, setYear] = useState(years[0]);
+
+  return (
+    <Listbox value={selectedYear} onChange={setYear}>
+      <div className="relative w-full mt-2">
+        <ListboxButton className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-black sm:text-sm sm:leading-6">
+          <span className="ml-3 block truncate">{selectedYear.year}</span>
+          <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
+            <ChevronUpDownIcon
+              aria-hidden="true"
+              className="h-5 w-5 text-gray-400"
+            />
+          </span>
+        </ListboxButton>
+
+        <ListboxOptions
+          transition
+          className="w-full absolute mt-1 max-h-56 overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm"
+        >
+          {years.map((year) => (
+            <ListboxOption
+              key={year.year}
+              value={year}
+              className="group w-full relative cursor-pointer select-none py-2 pl-3 pr-3 text-black data-[focus]:bg-slate-800 data-[focus]:text-white"
+            >
+              <span className="flex items-center w-full justify-center">
+                {year.year}
+              </span>
+              <span className="absolute inset-y-0 right-0 flex items-center pr-4 text-black group-data-[focus]:text-white [.group:not([data-selected])_&]:hidden">
+                <CheckIcon
+                  aria-hidden="true"
+                  className="h-5 w-5  text-black group-data-[focus]:text-white"
+                />
+              </span>
+            </ListboxOption>
+          ))}
+        </ListboxOptions>
+      </div>
+    </Listbox>
+  );
+}
+
+export default YearList;
--- src/pages/compareplayer.jsx
+++ src/pages/compareplayer.jsx
@@ -1,73 +1,42 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import API from "../component/api";
-import {
-  Listbox as HeadlessListbox, // Renaming Listbox to avoid conflict
-  ListboxButton,
-  ListboxOption,
-  ListboxOptions,
-} from "@headlessui/react";
-import { useState } from "react";
-
-const people = [
-  { id: 1, name: "Durward Reynolds" },
-  { id: 2, name: "Kenton Towne" },
-  { id: 3, name: "Therese Wunsch" },
-  { id: 4, name: "Benedict Kessler" },
-  { id: 5, name: "Katelyn Rohan" },
-];
-
-function Listbox() {
-  const [selectedPerson, setSelectedPerson] = useState(people[0]);
-  return (
-    <HeadlessListbox value={selectedPerson} onChange={setSelectedPerson}>
-      <ListboxButton>{selectedPerson.name}</ListboxButton>
-      <ListboxOptions anchor="bottom">
-        {people.map((person) => (
-          <ListboxOption
-            key={person.id}
-            value={person}
-            className="data-[focus]:bg-blue-100"
-          >
-            {person.name}
-          </ListboxOption>
-        ))}
-      </ListboxOptions>
-    </HeadlessListbox>
-  );
-}
+import YearList from "../component/yearList";
 
 class ComparePlayer extends React.Component {
   render() {
     return (
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="flex justify-center gap-4 py-4 w-auto">
-          <input
-            id=""
-            name="player"
-            type="name"
-            required
-            autoComplete="name"
-            placeholder="player name"
-            className="block w-auto rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
-          />
-          <div className="flex justify-center gap-4 py-4 w-auto">
-            <Listbox />
+          <div>
+            <input
+              id=""
+              name="player"
+              type="name"
+              required
+              autoComplete="name"
+              placeholder="player name"
+              className="block w-auto rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+            />
+            <div className="flex justify-center gap-4 py-4 w-auto">
+              <YearList />
+            </div>
           </div>
-          <input
-            id=""
-            name="player"
-            type="name"
-            required
-            autoComplete="name"
-            placeholder="player name"
-            className="block w-auto rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
-          />
-          <div className="flex justify-center gap-4 py-4 w-auto">
-            <Listbox />
+          <div>
+            <input
+              id=""
+              name="player"
+              type="name"
+              required
+              autoComplete="name"
+              placeholder="player name"
+              className="block w-auto rounded-md border-0 px-1.5 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-slate-600 sm:text-sm sm:leading-6"
+            />
+            <div className="flex justify-center gap-4 py-4 w-auto">
+              <YearList />
+            </div>
           </div>
         </div>
-
         <button
           type="submit"
           className="block w-full rounded-md bg-black px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-stone-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-stone-900"
@@ -77,7 +46,6 @@ class ComparePlayer extends React.Component {
         <p className="text-center">
           Currently our data is only accurate between the years of 1993 to 2024
         </p>
-        <yearSearch />
       </div>
     );
   }
