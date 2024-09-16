acd763b0cf9cbc6f00fa9c3e2390cf1194eab15a
Steven Nguyen <105184434+SdbnCode@users.noreply.github.com>
2024-09-16 14:49:00 -0400

    Updated about page to have credits and cleaner look

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,19 +1,47 @@
+import { SpeakerXMarkIcon } from "@heroicons/react/24/outline";
 import React from "react";
-import { NavLink } from "react-router-dom";
 
 class About extends React.Component {
   render() {
     return (
-      <div className="p-16 flex flex-col justify-center text-center gap-4">
-        <p>
-          Like many other NBA fans I always wondered "Who is really the best?"
-        </p>
-        <p>
-          With my passion for sports and developing web applications in hand, I
-          sought to develop a better way in visualing data and deciding the age
-          old question of{" "}
-        </p>
-        <p className="text-2xl font-bold">WHO IS THE BEST OF ALL TIME</p>
+      <div className="bg-gray-50 min-h-screen py-16 px-6">
+        <div className="max-w-4xl mx-auto bg-white shadow-lg rounded-lg p-8 lg:p-12 text-center">
+          <h1 className="text-5xl font-extrabold text-gray-900 mb-6">
+            About the Project
+          </h1>
+          <p className="text-lg text-gray-700 mb-4">
+            With my passion for sports and developing web applications, I sought
+            to combine both and create a web application to settle the age-old
+            debate of
+          </p>
+          <p className="text-3xl font-bold text-red-600 mb-8">
+            WHO IS THE BEST OF ALL TIME!
+          </p>
+          <p className="text-lg text-gray-700 ">
+            I hoped you enjoyed using this application as much as I enjoyed
+            creating it!
+          </p>
+        </div>
+
+        <div className="max-w-4xl mx-auto bg-gray-100 shadow-lg rounded-lg mt-16 p-8 lg:p-12 text-center">
+          <h2 className="text-4xl font-bold text-gray-900 mb-6">Credits</h2>
+          <p className="text-lg text-gray-700 mb-4">
+            Special thanks to the creator of the free NBA GraphQL API, which
+            provided the foundation for this project.
+          </p>
+          <p className="text-lg text-gray-700 mb-4">
+            You can find the API and documentation at the{" "}
+            <a
+              className="font-bold text-blue-700 hover:text-blue-900 hover:underline"
+              href="https://documenter.getpostman.com/view/25652688/2sA2xjzBAS#b76f9a34-9439-491b-af8f-9ccff8a00c96"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              NBA GraphQL API website
+            </a>
+            .
+          </p>
+        </div>
       </div>
     );
   }
